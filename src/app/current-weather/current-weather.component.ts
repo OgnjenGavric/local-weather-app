34a7fd4d7cf379ad60138963daadd595a4e7b4ec
Ognjen Gavric <ognjengavric2@gmail.com>
2023-05-17 09:54:10 +0200

    Add first component

---
+++ src/app/current-weather/current-weather.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-current-weather',
+  templateUrl: './current-weather.component.html',
+  styleUrls: ['./current-weather.component.css']
+})
+export class CurrentWeatherComponent {
+
+}
